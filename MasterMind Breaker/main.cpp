d13834f19c3bc8bcedec7ffbcfffc36f673759f4
Sacha Kozma <sacha.kozma@AA18-10.local>
2015-05-19 11:46:43 +0200

    First commit

--- MasterMind Breaker/main.cpp
+++ MasterMind Breaker/main.cpp
@@ -8,11 +8,46 @@
 
 #include <iostream>
 
+using namespace std;
+
+void genererCombinaison (int combinaisonCourante[], int nombreCases, int nombreCouleurs);
+void afficherCombinaison(int combinaisonCourante[], int nombreCases)
+;
+
+
 int main(int argc, const char * argv[])
 {
+    srandomdev();
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int nombreCases, longueurTableau, essais[longueurTableau][nombreCases], score[longueurTableau][2], nombreEssais = 0, combinaisonCourante[nombreCases], nombreCouleurs ;
+    
+    
+    genererCombinaison(combinaisonCourante, nombreCases, nombreCouleurs);
+    
+    afficherCombinaison(combinaisonCourante, nombreCases);
+    
+    cout << "Combien y a-t-il de noirs ?";
+    cin >> score[nombreEssais][0];
+    
+    cout << "Combien y a-t-il de blancs ?";
+    cin >> score[nombreEssais][1];
+    
     return 0;
 }
 
+void genererCombinaison (int combinaisonCourante[], int nombreCases, int nombreCouleurs)
+{
+    for (int i = 0; i < nombreCases; i++) {
+        combinaisonCourante[i] = random() % nombreCouleurs + 1;
+    }
+}
+
+void afficherCombinaison(int combinaisonCourante[], int nombreCases)
+{
+    for (int i = 0; i < nombreCases; i++) {
+        cout << " | " << combinaisonCourante[i] << " | ";
+    }
+    
+    cout << endl << endl;
+}

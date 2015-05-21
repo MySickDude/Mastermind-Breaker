e8a10d2ff73e28b43a0c151089063b317588780c
Sacha Kozma <sacha.kozma@AA18-10.local>
2015-05-21 10:51:22 +0200

    Second commit

--- MasterMind Breaker/main.cpp
+++ MasterMind Breaker/main.cpp
@@ -11,27 +11,59 @@
 using namespace std;
 
 void genererCombinaison (int combinaisonCourante[], int nombreCases, int nombreCouleurs);
-void afficherCombinaison(int combinaisonCourante[], int nombreCases)
-;
+void afficherCombinaison(int combinaisonCourante[], int nombreCases);
+int demanderNombre (string caption);
+void calculScore(int combinaisonMystere[], int combinaisonUtilisateur[], int nombreCases, int score []);
 
 
 int main(int argc, const char * argv[])
 {
     srandomdev();
-
     
-    int nombreCases, longueurTableau, essais[longueurTableau][nombreCases], score[longueurTableau][2], nombreEssais = 0, combinaisonCourante[nombreCases], nombreCouleurs ;
-    
+    const int nombreCases = demanderNombre("Combien de cases souhaitez-vous ?"), nombreCouleurs = demanderNombre("Combien de couleurs souhaitez-vous ?");
+    int  longueurTableau = 100, essais[longueurTableau][nombreCases], score[longueurTableau][2], combinaisonCourante[nombreCases], scoreTemp[2] = {0};
     
     genererCombinaison(combinaisonCourante, nombreCases, nombreCouleurs);
     
-    afficherCombinaison(combinaisonCourante, nombreCases);
     
-    cout << "Combien y a-t-il de noirs ?";
-    cin >> score[nombreEssais][0];
+    for (int nombreEssais = 0; score[nombreEssais][0] != 4; nombreEssais++) {
+        
+        for (int i = 0; i < nombreCases; i++) {
+            essais[nombreEssais][i] = combinaisonCourante[i];
+        }
+        
+        afficherCombinaison(combinaisonCourante, nombreCases);
+        
+        cout << "Combien y a-t-il de noirs ?";
+        cin >> score[nombreEssais][0];
+        
+        cout << "Combien y a-t-il de blancs ?";
+        cin >> score[nombreEssais][1];
+        
+        
+        while (scoreTemp[0] != score[nombreEssais][0] && scoreTemp[1] != score[nombreEssais][1]) {
+            fill(scoreTemp, scoreTemp + 2, 0);
+            
+            genererCombinaison(combinaisonCourante, nombreCases, nombreCouleurs);
+            
+            //Calculer le score !!
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
     
-    cout << "Combien y a-t-il de blancs ?";
-    cin >> score[nombreEssais][1];
+    
     
     return 0;
 }
@@ -50,4 +82,48 @@ void afficherCombinaison(int combinaisonCourante[], int nombreCases)
     }
     
     cout << endl << endl;
+}
+
+int demanderNombre (string caption)
+{
+    int nombre;
+    cout << caption;
+    cin >> nombre;
+    return nombre;
+}
+
+void calculScore(int combinaisonMystere [], int combinaisonUtilisateur [], int nombreCases, int score []) // Changer pour utiliser tableau bi-dimensionel ! (changer nom variable + envoyer nombreEssais)
+{
+    bool combinaisonMystereMarque [nombreCases], combinaisonUtilisateurMarque [nombreCases];
+    
+    
+    fill(score, score + 2, 0); // Remet à zéro les scores
+    
+    for (int i = 0; i < nombreCases; i++) { //Remise à zéro des marques sur les combinaisons
+        combinaisonMystereMarque[i] = false;
+        combinaisonUtilisateurMarque[i] = false;
+    }
+    
+    for (int i = 0; i < nombreCases; i++) { // Calcul du nombre de "noir" (bonne couleur au bon endroit)
+        if (combinaisonUtilisateur [i] == combinaisonMystere [i]) {
+            score [0] += 1; // score[0] représente les noirs
+            combinaisonMystereMarque[i] = true;
+            combinaisonUtilisateurMarque[i] = true;
+        }
+    }
+    
+    for (int i = 0; i < nombreCases; i++) { // Calcul du nombre de "blanc" (bonne couleur pas au même endroit)
+        if (not combinaisonMystereMarque[i]) {
+            for (int j = 0; j < nombreCases; j++) {
+                if (combinaisonUtilisateur[j] == combinaisonMystere[i] && not combinaisonUtilisateurMarque[j]) {
+                    score[1] += 1; // représente les blancs
+                    combinaisonMystereMarque[i] = true;
+                    combinaisonUtilisateurMarque[j] = true;
+                    break;
+                }
+            }
+        }
+        
+    }
+    
 }
